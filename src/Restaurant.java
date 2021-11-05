f1db65043a1af0f832b10169752fd0c8a080de00
arronpro666 <63225429+arronpro666@users.noreply.github.com>
2021-11-05 23:48:43 +0800

    update print menu

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -16,9 +16,6 @@ public class Restaurant {
 
 		int option;
 		Scanner sc = new Scanner(System.in);
-		
-		//Data List
-		restaMenu.printer();
 
 		while (true) {
 			System.out.println("SELECT OPTION: ");
