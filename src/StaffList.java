b4d637d7bd27edb73629e97a396b6ffa0590a7d9
cel-ong <celestineoxr@gmail.com>
2021-11-08 22:34:51 +0800

    edited naming

--- src/StaffList.java
+++ src/StaffList.java
@@ -9,26 +9,26 @@ public class StaffList implements PersonManager{
 	public StaffList() {
 		staffList = new ArrayList<Staff>(); 
 
-		Staff tempstaff1 = new Staff("John", 12345678, numStaff++, jobTitle.Cashier, gender.Male);
-		staffList.add(tempstaff1);
-		Staff tempstaff2 = new Staff("Tom", 22345678, numStaff++, jobTitle.Cashier, gender.Male);
-		staffList.add(tempstaff2);
-		Staff tempstaff3 = new Staff("Harry", 32345678, numStaff++, jobTitle.Waiter, gender.Male);
-		staffList.add(tempstaff3);
-		Staff tempstaff4 = new Staff("Jim", 42345678, numStaff++, jobTitle.Waiter, gender.Male);
-		staffList.add(tempstaff4);
-		Staff tempstaff5 = new Staff("Ryan", 52345678, numStaff++, jobTitle.Waiter, gender.Male);
-		staffList.add(tempstaff5);
-		Staff tempstaff6 = new Staff("Emma", 62345678, numStaff++, jobTitle.Waiter, gender.Female);
-		staffList.add(tempstaff6);
-		Staff tempstaff7 = new Staff("Ava", 72345678, numStaff++, jobTitle.Waiter, gender.Female);
-		staffList.add(tempstaff7);
-		Staff tempstaff8 = new Staff("Charlotte", 82345678, numStaff++, jobTitle.Chef, gender.Female);
-		staffList.add(tempstaff8);
-		Staff tempstaff9 = new Staff("Olivia", 92345678, numStaff++, jobTitle.Chef, gender.Female);
-		staffList.add(tempstaff9);
-		Staff tempstaff10 = new Staff("Sophia", 13345678, numStaff++, jobTitle.Manager, gender.Female);
-		staffList.add(tempstaff10);
+		Staff tempStaff1 = new Staff("John", 12345678, numStaff++, jobTitle.Cashier, gender.Male);
+		staffList.add(tempStaff1);
+		Staff tempStaff2 = new Staff("Tom", 22345678, numStaff++, jobTitle.Cashier, gender.Male);
+		staffList.add(tempStaff2);
+		Staff tempStaff3 = new Staff("Harry", 32345678, numStaff++, jobTitle.Waiter, gender.Male);
+		staffList.add(tempStaff3);
+		Staff tempStaff4 = new Staff("Jim", 42345678, numStaff++, jobTitle.Waiter, gender.Male);
+		staffList.add(tempStaff4);
+		Staff tempStaff5 = new Staff("Ryan", 52345678, numStaff++, jobTitle.Waiter, gender.Male);
+		staffList.add(tempStaff5);
+		Staff tempStaff6 = new Staff("Emma", 62345678, numStaff++, jobTitle.Waiter, gender.Female);
+		staffList.add(tempStaff6);
+		Staff tempStaff7 = new Staff("Ava", 72345678, numStaff++, jobTitle.Waiter, gender.Female);
+		staffList.add(tempStaff7);
+		Staff tempStaff8 = new Staff("Charlotte", 82345678, numStaff++, jobTitle.Chef, gender.Female);
+		staffList.add(tempStaff8);
+		Staff tempStaff9 = new Staff("Olivia", 92345678, numStaff++, jobTitle.Chef, gender.Female);
+		staffList.add(tempStaff9);
+		Staff tempStaff10 = new Staff("Sophia", 13345678, numStaff++, jobTitle.Manager, gender.Female);
+		staffList.add(tempStaff10);
 	}
 
 	public void add_Person()
@@ -38,9 +38,9 @@ public class StaffList implements PersonManager{
 		jobTitle tempTitle = jobTitle.Cashier;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter staff name: ");
-		String tempname = sc.next();
+		String tempName = sc.next();
 		System.out.println("Please enter staff phoneNum: ");
-		int tempnum = sc.nextInt();
+		int tempNum = sc.nextInt();
 		System.out.println("Please enter staff title: ");
 		while(flag)
 		{
@@ -74,7 +74,7 @@ public class StaffList implements PersonManager{
 			}
 		}
 		flag = true;
-		gender tempgender = gender.Male;
+		gender tempGender = gender.Male;
 		System.out.println("Please enter staff gender: ");
 		while(flag)
 		{
@@ -83,12 +83,12 @@ public class StaffList implements PersonManager{
 			switch(choice)
 			{
 				case 1:
-					tempgender = gender.Male;
+					tempGender = gender.Male;
 					flag = false;
 					break;
 				
 				case 2:
-					tempgender = gender.Female;
+					tempGender = gender.Female;
 					flag = false;
 					break;
 
@@ -97,8 +97,8 @@ public class StaffList implements PersonManager{
 					break;
 			}
 		}
-		Staff tempstaff = new Staff(tempname, tempnum, numStaff++, tempTitle, tempgender);
-		staffList.add(tempstaff);
+		Staff tempStaff = new Staff(tempName, tempNum, numStaff++, tempTitle, tempGender);
+		staffList.add(tempStaff);
 	}
 
 	public void delete_Person(int staffID) {
