9733eb32f751cddbc301d95a70bd4922d350d691
kombucha7 <chanhuide@gmail.com>
2021-11-13 16:07:47 +0800

    Added JavaDocs

--- src/Member.java
+++ src/Member.java
@@ -1,7 +1,18 @@
+/**
+ * Member class which inherits from the person class Type of customer that is
+ * able to enjoy the member discount
+ */
 public class Member extends Person {
 
-	public Member(String name, int phoneNum) {	//removed bool membership
-		super(name,phoneNum);
+	/**
+	 * Constructor for member object which takes in the name and phone number of the
+	 * person It calls the super constructor(person) to create the object
+	 * 
+	 * @param name     of the person to be created
+	 * @param phoneNum of the person to be created
+	 */
+	public Member(String name, int phoneNum) {
+		super(name, phoneNum);
 	}
 
 }
