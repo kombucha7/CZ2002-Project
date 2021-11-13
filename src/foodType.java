21826c6cf1f5ca0772c0eea9e41aea5898bd0cc3
kombucha7 <chanhuide@gmail.com>
2021-11-13 21:12:22 +0800

    Added javadocs

--- src/foodType.java
+++ src/foodType.java
@@ -1,6 +1,25 @@
+/**
+ * foodType enum for 4 food categories
+ */
 public enum foodType {
+
+	/**
+	 * Main category of food types
+	 */
 	Main,
-	Dessert,
-	Drinks,
+
+	/**
+	 * Dessert category of food types
+	 */
+	Dessert, 
+
+	/**
+	 * Drinks category of food types
+	 */
+	Drinks, 
+
+	/**
+	 * Appetizer category of food types
+	 */
 	Appetizer
 }
