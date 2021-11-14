6f4dae6ff858c474a9924fac42f10912e1ea4a95
cel-ong <celestineoxr@gmail.com>
2021-11-14 19:39:09 +0800

    removed some code

--- src/TableList.java
+++ src/TableList.java
@@ -21,37 +21,6 @@ public class TableList implements ListPrinter, Serializable {
 	 */
 	public TableList() {
 		tableList = new ArrayList<>();
-
-		Table table1 = new Table(0, 2);
-		tableList.add(table1);
-		Table table2 = new Table(1, 2);
-		tableList.add(table2);
-		Table table3 = new Table(2, 2);
-		tableList.add(table3);
-		Table table4 = new Table(3, 4);
-		tableList.add(table4);
-		Table table5 = new Table(4, 4);
-		tableList.add(table5);
-		Table table6 = new Table(5, 4);
-		tableList.add(table6);
-		Table table7 = new Table(6, 6);
-		tableList.add(table7);
-		Table table8 = new Table(7, 6);
-		tableList.add(table8);
-		Table table9 = new Table(8, 6);
-		tableList.add(table9);
-		Table table10 = new Table(9, 8);
-		tableList.add(table10);
-		Table table11 = new Table(10, 8);
-		tableList.add(table11);
-		Table table12 = new Table(11, 8);
-		tableList.add(table12);
-		Table table13 = new Table(12, 10);
-		tableList.add(table13);
-		Table table14 = new Table(13, 10);
-		tableList.add(table14);
-		Table table15 = new Table(14, 10);
-		tableList.add(table15);
 	}
 
 	/**
